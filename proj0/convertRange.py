bdd61f6adb2f52e88d5e86a22c5fbf0f499d4d1a
Luiz Cartolano <duducartolano@gmail.com>
2018-04-13 19:30:30 -0300

    Adding a code to convert the image range

--- proj0/convertRange.py
+++ proj0/convertRange.py
@@ -1,26 +1,25 @@
 import cv2
+from numpy import rint
 
 def negative(filename):
     path = '/Users/luizeduardocartolano/Dropbox/DUDU/Unicamp/IC/MC920/workspace/images/'
     image = path + filename
     # open the image
     img = cv2.imread(image,0)
-    # invert the image
-    img = cv2.bitwise_not(img)
-    # call the function that create the new modified image
-    showImage(filename, img)
+    img = rint(img * (60.0/255.0)) + 120
 
+    showImage(filename,img)
 
 def showImage(filename, image):
     # show the inverted image
-    newFile = '/Users/luizeduardocartolano/Dropbox/DUDU/Unicamp/IC/MC920/workspace/modifiedImages/'
+    newFile = '/Users/luizeduardocartolano/Dropbox/DUDU/Unicamp/IC/MC920/workspace/modifiedImages/range/'
     newFile = newFile + filename
     cv2.imwrite(newFile, image)
+
 
 def main():
     # setting the path to image
     filename = str(input("Enter the image name: "))
-
     negative(filename)
 
 if __name__ == '__main__':
