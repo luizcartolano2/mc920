8516f8f3dbebbe7b139e0472afb24376952ce49a
Luiz Cartolano <duducartolano@gmail.com>
2018-04-13 22:20:17 -0300

    Modifying the main to go throug all the files in a given directory

--- proj0/main.py
+++ proj0/main.py
@@ -1,21 +1,24 @@
 from manageImage import ManageImage
+import os
 
 def main():
 
     path = '/Users/luizeduardocartolano/Dropbox/DUDU/Unicamp/IC/MC920/workspace/images/'
-    filename = str(input("Enter the image name: "))
-    manager = ManageImage(path,filename)
+    #filename = str(input("Enter the image name: "))
 
-    # here we call the method that reads the png image
-    image = manager.readImage()
-    # here we plot the histogram of the given image
-    manager.imageHistogram(image)
-    # here we plot the statics of the given image
-    manager.imageStatics(image)
-    # here we plot the negative of an image
-    manager.imageNegative(image)
-    # here we change the range color of the image
-    manager.convertImageRange(image,120)
+    for filename in os.listdir(path):
+        # invoke the object
+        manager = ManageImage(path,filename)
+        # here we call the method that reads the png image
+        image = manager.readImage()
+        # here we plot the histogram of the given image
+        manager.imageHistogram(image)
+        # here we plot the statics of the given image
+        manager.imageStatics(image)
+        # here we plot the negative of an image
+        manager.imageNegative(image)
+        # here we change the range color of the image
+        manager.convertImageRange(image,120)
 
 if __name__ == '__main__':
     main()
