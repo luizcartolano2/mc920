03868bf072ec8ed9ca24fa6d109ef42100157b08
Luiz Cartolano <duducartolano@gmail.com>
2018-12-12 19:32:46 -0200

    add comments to the image management library

--- proj3/basicImage.py
+++ proj3/basicImage.py
@@ -2,20 +2,20 @@ import cv2
 import numpy as np
 from matplotlib.pyplot import imshow, show
 
-######################################################################
-# 	Method to read an image 										 #
-#																	 #	
-#    Parameters 													 #
-#   ----------														 #
-#	    filename : str 												 #
-#	        The file location of the image 							 #
-#																	 #
-#   Returns 														 #
-#    -------														 #
-#	    list 														 #
-#	        A list of ints with the matrix of pixels of the image.   #
-######################################################################
 def readImage(filename):
+	"""
+
+		Method to store new images
+
+	    Parameters
+	    ----------
+		    filename : str
+		        The file location of the image
+	    Returns
+	    -------
+		    img : list
+		    	A list of ints with the matrix of pixels of the image
+    """
 	# read the image
 	image_matrix = cv2.imread(filename,1)
 	img = image_matrix.astype(dtype='uint8')
@@ -43,6 +43,21 @@ def storeImage(filename, image_matrix):
 	cv2.imwrite(filename, image_matrix)
 
 def rotateImage(imgMatrix, rotAngle):
+	"""
+
+		Method to rotate image in a given angle.
+
+	    Parameters
+	    ----------
+		    image_matrix : list
+		    	A list of ints with the matrix of pixels of the image
+			rotAngle : double
+				Image rotation angle
+	    Returns
+	    -------
+		    rotated : list
+		    	A list of ints with the matrix of pixels of the image
+    """
 
 	#	pega a altura e largura da imagem
 	height, width = imgMatrix.shape[:2]
