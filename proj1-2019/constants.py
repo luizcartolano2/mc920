c8227466f23a8318bd731d1e2d5f40d34034bc91
Luiz Cartolano <duducartolano@gmail.com>
2019-04-09 17:29:09 -0300

    adicionando os inputs a pasta especifica do projeto 1 para que ela possa ser compactada e transmitida para outras pessoas

--- proj1-2019/constants.py
+++ proj1-2019/constants.py
@@ -10,5 +10,5 @@ import os
 #   paths para a pasta
 PROJ_PATH = os.getcwd()
 WKS_PATH = os.path.abspath(os.path.join(PROJ_PATH, os.pardir))
-IMAGE_PATH = WKS_PATH + '/images/'
+IMAGE_PATH = PROJ_PATH + '/inputs/'
 OUTPUT_PATH = PROJ_PATH + '/outputs/'
